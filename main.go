72453938b092e9f9534e66c4b536e7abf4f12dcf
john-zzzz <38539197+john-zzzz@users.noreply.github.com>
2018-11-26 17:24:26 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello you keyboard monkey, you"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
